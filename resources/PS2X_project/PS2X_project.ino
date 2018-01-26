8cd00e2575930c8e101df5beb8259b3a1e8f18d1
Manuel Alejandro Diaz Zapata <manuel.alejandro.diaz.zapata@gmail.com>
2018-01-26 15:07:45 -0500

    Add setup configuration for resources/PS2X_project/PS2X_project.ino

--- resources/PS2X_project/PS2X_project.ino
+++ resources/PS2X_project/PS2X_project.ino
@@ -10,17 +10,69 @@
 //#define rumble      true
 #define rumble      false
 
+PS2X ps2x; // create PS2 Controller Class
+
+//right now, the library does NOT support hot pluggable controllers, meaning
+//you must always either restart your Arduino after you connect the controller,
+//or call config_gamepad(pins) again after connecting the controller.
+
+int error = 0;
+byte type = 0;
+byte vibrate = 0;
+
 void setup() {
 
-  PS2X ps2x; // create PS2 Controller Class
+  Serial.begin(57600);
 
-  //right now, the library does NOT support hot pluggable controllers, meaning
-  //you must always either restart your Arduino after you connect the controller,
-  //or call config_gamepad(pins) again after connecting the controller.
+  delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
 
-  int error = 0;
-  byte type = 0;
-  byte vibrate = 0;
+  //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
+
+  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+
+  if (error == 0) {
+    Serial.print("Found Controller, configured successful ");
+    Serial.print("pressures = ");
+    if (pressures)
+      Serial.println("true ");
+    else
+      Serial.println("false");
+    Serial.print("rumble = ");
+    if (rumble)
+      Serial.println("true)");
+    else
+      Serial.println("false");
+    Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+    Serial.println("holding L1 or R1 will print out the analog stick values.");
+    Serial.println("Note: Go to www.billporter.info for updates and to report bugs.");
+  }
+  else if (error == 1)
+    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+
+  else if (error == 2)
+    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+
+  else if (error == 3)
+    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+
+  //  Serial.print(ps2x.Analog(1), HEX);
+
+  type = ps2x.readType();
+  switch (type) {
+    case 0:
+      Serial.print("Unknown Controller type found ");
+      break;
+    case 1:
+      Serial.print("DualShock Controller found ");
+      break;
+    case 2:
+      Serial.print("GuitarHero Controller found ");
+      break;
+    case 3:
+      Serial.print("Wireless Sony DualShock Controller found ");
+      break;
+  }
 
 }
 
